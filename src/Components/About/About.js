edab19433a65b8a4dfb35a8be6b0e927b3f4bd92
octaviofitz <octaviofitz@gmail.com>
2022-01-19 03:27:26 -0300

    add about, fix contact

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,8 +5,19 @@ import BannerIMG from '../Banner/BannerIMG/BannerIMG'
 
 function About() {
     return (
-        <section>  
+        <section className='about'>  
         <BannerIMG />
+        <div className='container-about'>
+        <div className='container-data-about'>
+          <h1 className='title-about'>Sobre nosotros</h1>
+          <p className='data-about'>Somos <b>Tu Notebook</b>, una empresa creada en el año 2020. Estamos ubicados en el barrio Adrogué, zona sur de la Provincia de Buenos Aires.</p>
+        </div>
+
+        <div className='container-mapa-about'>
+         
+          <img src='/map.jpg' alt='map-about' className='map-about'/>
+        </div>
+        </div>
         </section>
        )
      }
